c060a98d1aac0d89ae47169af65f4f32379e59d9
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2025-05-27 02:32:24 -0400

    state

--- setup-remote-state/main.tf
+++ setup-remote-state/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tfstate"
+  bucket = "ebritt07-tfstate"
      
   lifecycle {
     prevent_destroy = true
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "app-state"
+  name           = "ebritt07-app-state"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
