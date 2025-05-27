4015685b97c288b16223081765946ced15118de9
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2025-05-27 03:13:12 -0400

    local

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "${var.org_name}-tfstate"
+    bucket         = "${local.org_name}-tfstate"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "${var.org_name}-app-state"
+    dynamodb_table = "${local.org_name}-app-state"
     encrypt        = true
   }
 }
