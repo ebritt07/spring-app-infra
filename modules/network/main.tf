4c32b925287d7c4c7a4f8d5962df4e9b067bdaff
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2025-06-03 01:58:10 -0400

    add load balancer url output

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -128,6 +128,13 @@ resource "aws_security_group" "ecs_task_sg" {
     protocol  = "tcp"
     security_groups = [aws_security_group.lb.id]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 ### ECS Cluster
