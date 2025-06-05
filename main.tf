a5d95243a970888b7bb821c4c84fec3561d571cf
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2025-06-05 19:28:53 -0400

    des

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ module "spring_app_ecr_repo" {
   ecr_repo_name = var.spring_app_ecr_repo
 }
 
-module "network" {
-  source              = "./modules/network"
-  region              = var.region
-  spring_app_ecr_repo = var.spring_app_ecr_repo
-  spring_app_version  = var.spring_app_version
-  aws_account_id      = var.aws_account_id
-}
+# module "network" {
+#   source              = "./modules/network"
+#   region              = var.region
+#   spring_app_ecr_repo = var.spring_app_ecr_repo
+#   spring_app_version  = var.spring_app_version
+#   aws_account_id      = var.aws_account_id
+# }
